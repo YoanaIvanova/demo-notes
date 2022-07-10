2ccc372e184e587a60953bb333ee6a7a129f2381
Yoana Ivanova <yoana.ve.ivanova@gmail.com>
2022-07-10 16:13:46 +0300

    Simplify application

--- api/src/main/java/com/example/notes/api/DemoNotesApplication.java
+++ api/src/main/java/com/example/notes/api/DemoNotesApplication.java
@@ -2,11 +2,9 @@ package com.example.notes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@EnableJpaAuditing
 @SpringBootApplication
 public class DemoNotesApplication {
 
--- api/src/main/java/com/example/notes/api/model/Note.java
+++ api/src/main/java/com/example/notes/api/model/Note.java
@@ -2,21 +2,15 @@ package com.example.notes.api.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity(name = "note")
 public class Note {
 
@@ -32,13 +26,5 @@ public class Note {
 
     @Column(name = "isImportant")
     private boolean isImportant;
-
-    @CreatedDate
-    @Column(name = "created_date")
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(name = "last_modified_date")
-    private LocalDateTime lastModifiedDate;
 
 }
